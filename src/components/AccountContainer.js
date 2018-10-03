7b3b5dcd779b369a819b49e807cd2eba44b3b93b
ma539 <ma539@cornell.edu>
2018-10-03 10:19:03 -0400

    Finished challenge

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -3,24 +3,45 @@ import TransactionsList from './TransactionsList'
 import CategorySelector from './CategorySelector'
 import {transactions} from '../transactionsData'
 
+const URL = "https://boiling-brook-94902.herokuapp.com/transactions"
+
 class AccountContainer extends Component {
   constructor() {
     super()
-    //... your code here
+    this.state = {
+      transactions: [],
+      activeCategory: "All"
+    }
   }
 
-  handleChange() {
-    //... your code here
+  componentDidMount() {
+    fetch(URL)
+    .then(resp => resp.json())
+    .then(transactions => this.setState({ transactions }))
+  }
+
+  handleChange = (event) => {
+    // console.log(event.target.parentNode.children[1].innerText)
+    this.setState({
+      activeCategory: event.target.parentNode.children[1].innerText
+    })
+  }
+
+  filterTransactions = () => {
+    let currentTrans = this.state.transactions
+    if (this.state.activeCategory !== "All") {
+      currentTrans = currentTrans.filter(trans => trans.category === this.state.activeCategory)
+    }
+    return currentTrans
   }
 
   render() {
-    console.log(transactions)
     return (
       <div className="ui grid container">
 
-        <CategorySelector />
+        <CategorySelector handleChange={this.handleChange} activeCategory={this.state.activeCategory}/>
 
-        <TransactionsList />
+        <TransactionsList transactions={this.filterTransactions()}/>
 
       </div>
     )
